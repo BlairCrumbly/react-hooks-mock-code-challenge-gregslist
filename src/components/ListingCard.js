11d21e72086324b157257dd2db7d3e5768d8dbe9
Blair Crumbly <blaircrumbly@gmail.com>
2025-02-03 16:14:56 -0500

    displays all cards + favorites feature

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,24 +1,38 @@
 import React from "react";
+import { useState } from "react";
 
-function ListingCard() {
-  return (
-    <li className="card">
-      <div className="image">
-        <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
-      </div>
-      <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
-        ) : (
-          <button className="emoji-button favorite">☆</button>
-        )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
-      </div>
-    </li>
-  );
-}
 
-export default ListingCard;
+  function ListingCard({ id, image, location, description }) {
+    const [isFavorite, setIsFavorite] = useState(false);
+  
+    function handleFavoriteClick() {
+      setIsFavorite((prev) => !prev); // Toggle between true and false
+    }
+  
+    return (
+      <li className="card">
+        <div className="image">
+          <span className="price">$0</span>
+          <img src={image} alt={description} />
+        </div>
+        <div className="details">
+          
+          <button 
+            className={`emoji-button favorite ${isFavorite ? "active" : ""}`} 
+            onClick={handleFavoriteClick}
+          >
+            {isFavorite ? "★" : "☆"}
+          </button>
+
+          <strong>{description}</strong>
+          <span> · {location}</span>
+
+
+          
+          <button className="emoji-button delete">🗑</button>
+        </div>
+      </li>
+    );
+  }
+
+  export default ListingCard;
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,14 +1,35 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState } from "react";
+import ListingCard from "./ListingCard";
+import { useEffect } from "react";
+
 
 function ListingsContainer() {
+
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+      .then((response) => response.json())
+      .then((listings) => setListings(listings))
+      .catch((error) => console.error("Error fetching listings:", error));
+  }, []);
+
+  const handleDeleteListings = () =>{
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: "DELETE",
+    })
+  }
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listings.map((listing) => (
+          <ListingCard key={listing.id} {...listing}/>
+        ))}
       </ul>
     </main>
   );
 }
 
 export default ListingsContainer;
+
